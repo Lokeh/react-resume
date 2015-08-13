9a9021af0c13d065c082693cda38e0d6f4f34951
Will <will@willacton.com>
2015-08-13 10:46:26 -0700

    Resume react component

--- src/js/init.jsx
+++ src/js/init.jsx
@@ -1,4 +1,24 @@
 // init
 
 const React = require('react');
+const getResumeJSON = require('./libs/getResumeJSON');
+
+const Resume = React.createClass({
+	getInitialState() {
+		return { resume: {} };
+	},
+	componentWillMount() {
+		getResumeJSON()
+			.then((resume) => this.setState({ resume }))
+			.catch((...err) => console.error(...err));
+	},
+	render() {
+		console.log(this.state.resume);
+		return(
+			<h1>Hello, world!</h1>
+		);
+	}
+});
+
+React.render(<Resume />, document.getElementById('app'));
 
