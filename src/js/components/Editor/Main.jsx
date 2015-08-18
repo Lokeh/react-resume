b3258c121058d5ca49ea1f8546342e5a0ea3fa06
Will <will@willacton.com>
2015-08-18 10:07:42 -0700

    undo redo buttons

--- src/js/components/Editor/Main.jsx
+++ src/js/components/Editor/Main.jsx
@@ -8,6 +8,7 @@ const ResumeModel = require('../../models/ResumeModel');
 const HistoryModel = require('../../models/HistoryModel');
 
 const Entry = require('./Entry.jsx');
+const Toolbar = require('./Toolbar.jsx');
 
 const Editor = React.createClass({
 	getInitialState() {
@@ -32,6 +33,9 @@ const Editor = React.createClass({
 	render() {
 		return (
 			<div style={{margin: "5px 10px"}}>
+				<div id="toolbar">
+					<Toolbar />
+				</div>
 				<Entry value={this.state.resume} keyName="resume" path="" minEditDepth={0} minRemovalDepth={1} />
 				<button onClick={this._onClick}>History</button>
 			</div>
--- src/js/components/Editor/Toolbar.jsx
+++ src/js/components/Editor/Toolbar.jsx
@@ -1,1 +1,38 @@
 // Toolbar
+const React = require('react');
+
+// Material UI
+const mui = require('material-ui');
+const ThemeManager = new mui.Styles.ThemeManager();
+const Colors = mui.Styles.Colors;
+const {Toolbar, ToolbarGroup, ToolbarTitle, ToolbarSeparator, RaisedButton, FontIcon, DropDownMenu, DropDownIcon} = mui;
+
+const ResumeModel = require('../../models/ResumeModel');
+const HistoryModel = require('../../models/HistoryModel');
+
+const EditorToolBar = React.createClass({
+	childContextTypes: {
+		muiTheme: React.PropTypes.object
+	},
+	getChildContext() {
+		return {
+			muiTheme: ThemeManager.getCurrentTheme()
+		};
+	},
+	undo() {
+
+	},
+	redo() {
+
+	},
+	render() {
+		return (
+			<div>
+					<div style={{marginTop: "5px"}}><RaisedButton label="UNDO" primary onClick={this.undo} /></div>
+					<div style={{marginTop: "5px"}}><RaisedButton label="REDO" secondary onClick={this.redo} /></div>
+			</div>
+		);
+	}
+});
+
+module.exports = EditorToolBar;
