54fb402e92fe338c8b6a2540c0fd11e351199b75
Will <will@willacton.com>
2015-08-25 11:03:05 -0700

    cursor set and delete

--- src/js/components/Editor/Entry.jsx
+++ src/js/components/Editor/Entry.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const Immutable = require('immutable');
+const Cursor = require('immutable/contrib/cursor');
 const {List, Map} = Immutable;
 
 const AddMapEntry = require('./AddMapEntry.jsx');
@@ -40,7 +41,8 @@ const Entry = React.createClass({
 	getInitialState() {
 		return {
 			collapsed: false,
-			inputValue: ""
+			inputValue: "",
+			cursor: null
 		};
 	},
 	parsePath(path) {
@@ -53,39 +55,46 @@ const Entry = React.createClass({
 	},
 	_onBlur(e) {
 		// update the model on blur
-		this.props.saveFn(this.parsePath(this.props.path), this.state.inputValue);
+		// this.props.saveFn(this.parsePath(this.props.path), this.state.inputValue);
+		this.props.cursor.set(this.props.keyName, this.state.inputValue);
 	},
 	_onKeyUp(e) {
 		// update the model on enter
 		if (e.key === "Enter") {
-			this.props.saveFn(this.parsePath(this.props.path), this.state.inputValue);
+			// this.props.saveFn(this.parsePath(this.props.path), this.state.inputValue);
+			this.props.cursor.set(this.props.keyName, this.state.inputValue);
 		}
 	},
 	deletePath(e) {
 		e.preventDefault();
 		// this.props.deleteFn(this.parsePath(this.props.path));
-		this.props.cursor.set(null);
+		this.props.cursor.delete(this.props.keyName);
 	},
 	toggleCollapsed(e) {
 		e.preventDefault();
 		this.setState({ collapsed: !this.state.collapsed });
 	},
 	componentWillMount() {
-		this.setState({ inputValue: this.props.value });
+		this.setState({
+			inputValue: this.props.value
+		});
 	},
 	componentWillReceiveProps(nextProps) {
-		this.setState({ inputValue: nextProps.value });
+		this.setState({
+			inputValue: nextProps.value
+		});
 	},
 	shouldComponentUpdate(nextProps, nextState) {
 		return this.props.value !== nextProps.value || this.state.collapsed !== nextState.collapsed || this.state.inputValue !== nextState.inputValue
 	},
 	render() {
-		this.props.cursor ? console.log(this.props.cursor) : 0;
+		const cursor = this.props.keyName !== 'resume' ? this.props.cursor.get(this.props.keyName) : this.props.cursor;
 
+		console.log(this.props.keyName+': changed');
 		const value = this.props.value;
 		const isMinRemovalDepth = this.parsePath(this.props.path).length > this.props.minRemovalDepth;
-		const isMap = !!this.props.cursor['@@__IMMUTABLE_KEYED__@@']; // Map.isMap(value);
-		const isList = !!this.props.cursor['@@__IMMUTABLE_ORDERED__@@'] && !isMap; // List.isList(value)
+		const isMap = !!cursor && !!cursor['@@__IMMUTABLE_KEYED__@@']; // Map.isMap(value);
+		const isList = !!cursor && !!cursor['@@__IMMUTABLE_ORDERED__@@'] && !isMap; // List.isList(value)
 		if (this.state.collapsed === false) {
 			const isMinEditDepth = this.parsePath(this.props.path).length > this.props.minEditDepth;
 			return (
@@ -95,14 +104,14 @@ const Entry = React.createClass({
 					(isMap ?
 						(<span>{'{'} {(isMinRemovalDepth) ? <a href="#" onClick={this.deletePath}><i className="fa fa-times-circle" style={{color: "#FD971F"}} /></a> : '' }
 							{value.map((v, k) => {
-								return (<Entry cursor={this.props.cursor.get(k)} saveFn={this.props.saveFn} deleteFn={this.props.deleteFn} getFn={this.props.getFn} key={k} value={v} keyName={k} path={this.props.path+k+'.'} minEditDepth={this.props.minEditDepth} minRemovalDepth={this.props.minRemovalDepth} />);
+								return (<Entry cursor={cursor} saveFn={this.props.saveFn} deleteFn={this.props.deleteFn} getFn={this.props.getFn} key={k} value={v} keyName={k} path={this.props.path+k+'.'} minEditDepth={this.props.minEditDepth} minRemovalDepth={this.props.minRemovalDepth} />);
 							}).toList()}
 							{(isMinEditDepth) ? <AddMapEntry saveFn={this.props.saveFn} path={this.props.path} /> : ''}
 						{'}'}</span>) :
 					(isList ?
 						(<span>{'['} {(isMinRemovalDepth) ? <a href="#" onClick={this.deletePath}><i className="fa fa-times-circle" style={{color: "#FD971F"}} /></a> : '' }
 							{value.map((v, k) => {
-								return (<Entry cursor={this.props.cursor.get(k)} saveFn={this.props.saveFn} deleteFn={this.props.deleteFn} getFn={this.props.getFn} key={k} value={v} keyName={k} path={this.props.path+k+'.'} minEditDepth={this.props.minEditDepth} minRemovalDepth={this.props.minRemovalDepth} />);
+								return (<Entry cursor={cursor} saveFn={this.props.saveFn} deleteFn={this.props.deleteFn} getFn={this.props.getFn} key={k} value={v} keyName={k} path={this.props.path+k+'.'} minEditDepth={this.props.minEditDepth} minRemovalDepth={this.props.minRemovalDepth} />);
 							}).toList()} 
 							{(isMinEditDepth) ? <AddListEntry path={this.props.path} saveFn={this.props.saveFn} getFn={this.props.getFn} /> : ''}
 						{']'}</span>) :
